2ef9cda4e727d9c1ea02c356b0c1469d038a465e
Tony Edgin <tedgin@iplantcollaborative.org>
2017-07-06 15:51:28 -0700

    init-roles deletes previously installed roles
    ansible-galaxy won't update roles installed through dependencies even
    with the --force flag, so before installing roles, delete all currently
    installed roles.

--- init-roles.sh
+++ init-roles.sh
@@ -7,5 +7,11 @@ readonly PlaybookDir=$(dirname "$ExecName")/playbooks
 
 for roleDir in $(find "$PlaybookDir" -type d -name roles)
 do
+  # ansible-galaxy won't update roles installed through dependencies, so delete all roles before
+  # updating
+  for role in $(find "$roleDir" -maxdepth 1 -mindepth 1 -type d)
+  do
+    rm --force --recursive "$role"
+  done
   ansible-galaxy install --force --role-file="$roleDir"/requirements.yml --roles-path="$roleDir"
 done

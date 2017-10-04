6b6fe1d22764b6bd5ea65165127f33b6b0856cab
Tony Edgin <tedgin@iplantcollaborative.org>
2017-10-04 11:19:39 -0700

    CORE-9003 reverting to standard role locations

--- init-roles.sh
+++ init-roles.sh
@@ -3,14 +3,16 @@
 set -e
 
 readonly ExecName=$(readlink --canonicalize "$0")
-readonly RolesDir=$(dirname "$ExecName")/roles
+readonly PlaybooksDir=$(dirname "$ExecName")
 
-# ansible-galaxy won't update roles installed through dependencies, so delete all roles before
-# updating
-for role in $(find "$RolesDir" -maxdepth 1 -mindepth 1 -type d)
+for roleDir in $(find "$PlaybooksDir" -type d -name roles)
 do
-  rm --force --recursive "$role"
+  # ansible-galaxy won't update roles installed through dependencies, so delete all roles before
+  # updating
+  for role in $(find "$roleDir" -maxdepth 1 -mindepth 1 -type d)
+  do
+    rm --force --recursive "$role"
+  done
+
+  ansible-galaxy install --force --role-file="$roleDir"/requirements.yml --roles-path="$roleDir"
 done
-
-ansible-galaxy install --force --role-file="$RolesDir"/requirements.yml --roles-path="$RolesDir"
-

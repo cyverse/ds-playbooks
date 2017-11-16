986108981f79a3fe22fc2531be72394d7d3ef1c4
Tony Edgin <tedgin@iplantcollaborative.org>
2017-11-16 15:47:14 -0700

    CORE-9072 created version for CentOS 7

---
+++ testing/ansible-support/config.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+set -e
+
+yum --assumeyes install openssh-server
+yum clean all
+rm --force --recursive /var/cache/yum
+
+ssh-keygen -q -f /etc/ssh/ssh_host_key -N '' -t rsa
+ssh-keygen -q -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+ssh-keygen -q -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+
+sed --in-place '/session    required     pam_loginuid.so/d' /etc/pam.d/sshd
+
+cat <<EOF | sed --in-place --file - /etc/ssh/sshd_config
+s/PermitRootLogin without-password/PermitRootLogin yes/
+s/#PermitEmptyPasswords no/PermitEmptyPasswords yes/
+EOF
+
+mkdir --parents /root/.ssh
+chmod 700 /root/.ssh
+
+chpasswd <<< root:

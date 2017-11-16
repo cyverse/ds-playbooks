4072a08902ae142c364ee5bdf1d1d850da294379
Tony Edgin <tedgin@iplantcollaborative.org>
2017-11-16 13:02:28 -0700

    CORE-9049 switched to ansible-support for the base image

---
+++ testing/dbms/service.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+main()
+{
+  if [ "$#" -lt 1 ]
+  then
+    printf 'Requires one paramter' >&2
+    return 1
+  fi
+
+  local action="$1"
+
+  if [ "$action" != start -a "$action" != stop ]
+  then
+    printf 'Unknown command "%s"\n' "$action" >&2
+    return 1
+  fi
+
+  su --command "/usr/pgsql-9.3/bin/pg_ctl -w '$action'" --login postgres
+}
+
+
+set -e
+
+main "$@"

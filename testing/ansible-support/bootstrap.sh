f35d0229c5df6389ed4f439e5f144518169d879a
Tony Edgin <tedgin@iplantcollaborative.org>
2017-11-16 13:01:10 -0700

    CORE-9072 created base image for ansible managed containers

---
+++ testing/ansible-support/bootstrap.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+start()
+{
+  if [ -n "$cmd" ]
+  then
+    eval "$cmd" start
+  fi
+
+  service sshd start
+}
+
+
+stop()
+{
+  service sshd stop
+
+  if [ -m "$cmd" ]
+  then
+    eval "$cmd" stop
+  fi
+}
+
+
+main()
+{
+  local cmd="$*"
+
+  start "$cmd"
+  trap "kill \${!}; stop '$cmd'; exit \$?" SIGTERM
+
+  while true
+  do
+    tail --follow /dev/null &
+    wait ${!}
+  done
+}
+
+
+set -e
+
+main "$*"

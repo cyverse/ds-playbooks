72c301d00ed86c2ba9aa5657bbb2488824da0cf4
Tony Edgin <tedgin@iplantcollaborative.org>
2017-11-20 12:21:58 -0700

    CORE-8880 localized ansible-support build logic

--- testing/build.sh
+++ testing/build.sh
@@ -4,14 +4,8 @@ main()
 {
   local baseDir=$(dirname $(readlink -f "$0"))
 
-  docker build --file "$baseDir"/ansible-support/Dockerfile.centos6 --tag ansible-support:centos6 \
-               "$baseDir"/ansible-support
-
-  docker build --file "$baseDir"/ansible-support/Dockerfile.centos7 --tag ansible-support:centos7 \
-               "$baseDir"/ansible-support
-
+  "$baseDir"/ansible-support/build
   docker-compose --file "$baseDir"/env/docker-compose.yml --project-name dstesting build
-
   docker build --tag dstesting_ansible "$baseDir"/ansible
 }
 

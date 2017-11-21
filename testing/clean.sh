bfafd571cdca47ec5b78561b2306e44312ac3338
Tony Edgin <tedgin@iplantcollaborative.org>
2017-11-21 06:48:48 -0700

    CORE-8880 testing clean script calls ansible-support clean

--- testing/clean.sh
+++ testing/clean.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
+
 main()
 {
+  local baseDir=$(dirname $(readlink -f "$0"))
+
   docker rmi \
-    dstesting_ansible \
-    dstesting_rs_centos6 dstesting_rs_centos7 dstesting_ies dstesting_dbms \
-    ansible-support:centos6 ansible-support:centos7
+    dstesting_ansible dstesting_rs_centos6 dstesting_rs_centos7 dstesting_ies dstesting_dbms
+
+  "$baseDir"/ansible-support/clean
 }
 
 

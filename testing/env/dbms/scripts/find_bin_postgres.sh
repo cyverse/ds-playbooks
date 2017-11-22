867421dca165c12021cc0c76c8a27663338ecce2
Tony Edgin <tedgin@iplantcollaborative.org>
2017-11-22 08:04:32 -0700

    CORE-9072 envvar inclusion occurs in the files that use them

--- testing/env/dbms/scripts/find_bin_postgres.sh
+++ testing/env/dbms/scripts/find_bin_postgres.sh
@@ -1,14 +1,20 @@
-#!/bin/bash
+#! /bin/bash
 
-# find whatever psql is in the current user's path
-ret=`which psql`
-# if none found report a failure
-if [ "$ret" == "" ]; then
-    echo "No postgres [psql] found.  Aborting." 1>&2
-    ret="FAIL/FAIL"
-    exit 1
-fi
-# return the bin directory holding psql
-echo `dirname $ret`
-# exit cleanly
-exit 0
+
+main()
+{
+  # find whatever psql is in the current user's path
+  if ! psqlPath=$(which psql)
+  then
+    printf 'No postgres [psql] found. Aborting.\n' >&2
+    return 1
+  fi
+
+  # return the bin directory holding psql
+  dirname "$psqlPath"
+}
+
+
+set -e
+
+main

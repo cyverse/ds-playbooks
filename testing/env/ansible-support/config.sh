eb637b5e573105867e2ca235dd5ceabd140cef96
Tony Edgin <tedgin@iplantcollaborative.org>
2018-03-29 12:57:46 -0700

    CORE-9044 added support for debian in ansible-support

--- testing/env/ansible-support/config.sh
+++ testing/env/ansible-support/config.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 #
 # Usage:
-#  config.sh VERSION
+#  config.sh OS VERSION
 #
 # Parameter:
-#  VERSION  the CentOS major version to configure, either 6 or 7.
+#  OS       The name of the operating system being configured.
+#  VERSION  the major version of the OS to configure.
 #
-# This script configures the common ansible requirements for either CentOS
-# version.
+# This script configures the common ansible requirements
 #
 
 
@@ -15,38 +15,78 @@ main()
 {
   if [ "$#" -lt 1 ]
   then
-    printf 'The CentOS version number is required as the first argument\n' >&2
+    printf 'The OS name is required as the first argument\n' >&2
     return 1
   fi
 
+  if [ "$#" -lt 2 ]
+  then
+    printf 'The OS version number is required as the second argument\n' >&2
+    return 1
+  fi
+
+  local os="$1"
+  local version="$2"
+
+  # Install required packages
+  if [ "$os" = centos ]
+  then
+    install_centos_packages "$version"
+  else
+    install_debian_packages
+  fi
+
+  # Allow root to have login without a password
+  passwd -d root
+
+  # Configure passwordless root ssh access
+  ssh-keygen -q -f /etc/ssh/ssh_host_key -N '' -t rsa
+
+  if ! [ -e /etc/ssh/ssh_host_dsa_key ]
+  then
+    ssh-keygen -q -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+  fi
+
+  if ! [ -e /etc/ssh/ssh_host_rsa_key ]
+  then
+    ssh-keygen -q -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+  fi
+
+  sed --in-place '/session    required     pam_loginuid.so/d' /etc/pam.d/sshd
+  update_sshd_config
+  mkdir --parents /var/run/sshd
+  mkdir --mode 0700 /root/.ssh
+}
+
+
+install_centos_packages()
+{
   local version="$1"
 
   rpm --import file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-"$version"
   yum --assumeyes install epel-release
   rpm --import file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-"$version"
+
   yum --assumeyes install \
-      libselinux-python openssh-server python-pip python-requests python-virtualenv sudo
-  yum clean all
-  rm --force --recursive /var/cache/yum
+    libselinux-python openssh-server python-pip python-requests python-virtualenv sudo
+}
 
-  ssh-keygen -q -f /etc/ssh/ssh_host_key -N '' -t rsa
-  ssh-keygen -q -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
-  ssh-keygen -q -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
 
-  sed --in-place '/session    required     pam_loginuid.so/d' /etc/pam.d/sshd
-  update_sshd_config
-  mkdir --parents /root/.ssh
-  chmod 700 /root/.ssh
+install_debian_packages()
+{
+  apt-get update --quiet
+  apt-get install --no-install-recommends --quiet --yes apt-utils
 
-  chpasswd <<< root:
+  apt-get install --no-install-recommends --quiet --yes \
+    openssh-server python-pip python-selinux python-virtualenv sudo
 }
 
 
 update_sshd_config()
 {
-  cat <<EOF | sed --in-place --file - /etc/ssh/sshd_config
-s/PermitRootLogin without-password/PermitRootLogin yes/
-s/#PermitEmptyPasswords no/PermitEmptyPasswords yes/
+  cat <<EOF | sed --in-place  --regexp-extended --file - /etc/ssh/sshd_config
+s/#?PermitRootLogin .*/PermitRootLogin yes/
+s/#?PermitEmptyPasswords no/PermitEmptyPasswords yes/
 EOF
 }
 

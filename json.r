c957f00852c297f65dec6f4c58bdd9b95bac86fc
Tony Edgin <tedgin@iplantcollaborative.org>
2022-06-09 14:46:10 -0700

    bms: new rules WIP

--- json.r
+++ json.r
@@ -1,11 +1,24 @@
 json {
-  *t = deserialize(*Serial);
+  *t = deserialize('"bob"');
   writeLine('stdout', serialize(*t));
 }
 
-# 1. Backspaces and form feeds cannot be handled in Strings by this library.
+# 1. Only the control characters for horizontal tab, carriage return, and line 
+#    feed are supported in strings.
 # 2. exponential notation is not supported.
 # 3. Numbers may not begin with a . or a +.
+
+substrRem : string * int -> string
+substrRem(*String, *NewHdPos) = substr(*String, *NewHdPos, strlen(*String))
+
+strHd : string -> string
+strHd(*String) = substr(*String, 0, 1)
+
+strTl : string -> string
+strTl(*String) = substrRem(*String, 1)
+
+trimLeadingSpace : string -> string
+TODO implement
 
 append : string * string * list string -> string
 append(*Base, *Separator, *Elements) =
@@ -24,9 +37,6 @@ revAccum : list ? * list ? -> list ?
 revAccum(*RevList, *List) = 
   if size(*List) == 0 then *RevList else revAccum(cons(hd(*List), *RevList), tl(*List)) 
 
-trimlBlanks : string -> string
-trimlBlanks(*Str) = triml(triml(triml(triml(*Str, ' '), '\t'), '\n'), '\r')
-
 encode : string -> string
 encode(*Unencoded) = encodeAccum('', *Unencoded)
 
@@ -34,15 +44,15 @@ encodeAccum : string * string -> string
 encodeAccum(*Encoded, *Unencoded) =
   if strlen(*Unencoded) == 0 then *Encoded
   else
-    let *c = substr(*Unencoded, 0, 1) in
+    let *c = strHd(*Unencoded) in
     let *escC = 
       if *c == '"' then '\\"' 
-      else if *c == '\t' then '\\t' 
       else if *c == '\n' then '\\n' 
       else if *c == '\r' then '\\r' 
+      else if *c == '\t' then '\\t' 
       else if *c == '\\' then '\\\\' 
       else *c in 
-    encodeAccum(*Encoded ++ *escC, substr(*Unencoded, 1, strlen(*Unencoded)))
+    encodeAccum(*Encoded ++ *escC, strTl(*Unencoded))
 
 data json_val =
   | json_array : list json_val -> json_val
@@ -99,57 +109,76 @@ serializeScalarsAccum(*RevSerialized, *Unserialized) =
 
 deserialize : string -> json_val
 deserialize(*Serial) =
-  let *stripped = trimlBlanks(*Serial) in
-  let *h = substr(*stripped, 0, 1) in
+  let *trimmed = trimLeadingSpace(*Serial) in
+  let *h = strHd(*Serial) in
   let (*val, *remnants) =
-    if *h == '[' then deserializeArray(*stripped)
-    else if *h == 'f' || *h == 't' then deserializeBoolean(*stripped)
-    else if *h == 'n' then deserializeNull(*stripped)
-    else if *h like regex '^[-0-9]' then deserializeNumber(*stripped)
-    else if *h == '"' then deserializeString(*stripped)
-    else if *h == '{' then deserializeObject(*stripped) 
-    else mkDeserializeErrRes('there is no legal next value', *stripped) in
+    if *h == '[' then deserializeArray(*trimmed)
+    else if *h == 'f' || *h == 't' then deserializeBoolean(*trimmed)
+    else if *h == 'n' then deserializeNull(*trimmed)
+    else if *h like regex '[-0-9]' then deserializeNumber(*trimmed)
+    else if *h == '"' then deserializeString(*trimmed)
+    else if *h == '{' then deserializeObject(*trimmed) 
+    else mkDeserializeErrRes('there is no legal next value', *trimmed) in
   *val
+
+deserializeArray : string -> json_val * string
+# TODO implement
 
 deserializeBoolean : string -> json_val * string
 deserializeBoolean(*Serial) =
-  if *Serial like 'false*' then (json_bool(false), substr(*Serial, 5, strlen(*Serial)))
-  else if *Serial like 'true*' then (json_bool(true), substr(*Serial, 4, strlen(*Serial)))
+  if *Serial like 'false*' then (json_bool(false), substrRem(*Serial, 5))
+  else if *Serial like 'true*' then (json_bool(true), substrRem(*Serial, 4))
   else mkDeserializeErrRes('expected next value to be a Boolean', *Serial)
 
 deserializeNull : string -> json_val * string
 deserializeNull(*Serial) =
-  if *Serial like 'null*' then (json_null, substr(*Serial, 4, strlen(*Serial))) 
+  if *Serial like 'null*' then (json_null, substrRem(*Serial, 4)) 
   else mkDeserializeErrRes("expected next value to be 'null'", *Serial)
 
+# FORMAT -?[0-9]+(\.[0-9]*)?
 deserializeNumber : string -> json_val * string
-deserializeNumber(*Serial) = 
-TODO figure out why regex isn't matching
-  if *Serial like regex '^-?[0-9]' then deserializeNumberAccum('', *Serial)
+deserializeNumber(*Serial) =
+  if *Serial like regex '^-\{0,1\}[0-9].*' then 
+    let (*numStrBuf, *Serial) = 
+      if *Serial like '-*' then ('-', strTl(*Serial)) else ('', *Serial) in 
+    let (*numStrBuf, *Serial) = extractDigits(*numStrBuf, *Serial) in
+    let (*numStrBuf, *Serial) = 
+      if *Serial like '.*' then extractDigits(*numStrBuf ++ '.', strTl(*Serial))
+      else (*numStrBuf, *Serial) in
+    (json_num(double(*numStrBuf)), *Serial)
   else mkDeserializeErrRes("expected next value to be a number", *Serial)
 
-# FORMAT -?[0-9]+(\.[0-9]*)?
-deserializeNumberAccum : string * string -> json_val * string
-deserializeNumberAccum(*NumStrBuf, *Serial) =
-  let *curPos = 0 in
-  let *curChar = substr(*Serial, *curPos, *curPos + 1) in
-  let *_ = if (*curChar == '-') {
-    *NumStrBuf = *NumStrBuf ++ *curChar;
-    *curPos = *curPos + 1;
-    *curChar = substr(*Serial, *curPos, *curPos + 1) } in
-  let *_ = while (*curChar like regex '[0-9]') {
-    *NumStrBuf = *NumStrBuf ++ *curChar;
-    *curPos = *curPos + 1;
-    *curChar = substr(*Serial, *curPos, *curPos + 1) } in
-  let *_ = if (*curChar == '.') {
-    *NumStrBuf = *NumStrBuf ++ *curChar;
-    *curPos = *curPos + 1;
-    *curChar = substr(*Serial, *curPos, *curPos + 1) } in     
-  let *_ = while (*curChar like regex '[0-9]') {
-    *NumStrBuf = *NumStrBuf ++ *curChar;
-    *curPos = *curPos + 1;
-    *curChar = substr(*Serial, *curPos, *curPos + 1) } in
-  (json_num(double(*NumStrBuf)), substr(*Serial, *curPos, strlen(*Serial)))
+extractDigits : string * string -> string * string
+extractDigits(*Buf, *Serial) =
+  if *Serial == '' || !(*Serial like regex '^[0-9].*') then (*Buf, *Serial)
+  else extractDigits(*Buf ++ strHd(*Serial), strTl(*Serial)) 
+
+deserializeObject : string -> json_val * string
+# TODO implement
+
+deserializeString(*Serial) =
+  let *initSerial = *Serial in
+  let *buf = '' in
+  let *Serial = triml(*Serial, '"') in
+  let (*str, *Serial) = extractString('', *Serial) in
+  if *Serial like '"*' then (json_str(*str), strTl(*Serial))
+  else mkDeserializeErrRes("expected next value to be a string", *initSerial)
+
+extractString : string * string -> string * string
+extractString(*Buf, *Serial) =
+  if *Serial == '' || *Serial == '\\' || *Serial like '"*' then (*Buf, *Serial)
+  else 
+    let (*Buf, *Serial) =
+      if *Serial like '\\*' then
+        let *escChar = substr(*Serial, 0, 2) in
+        let *char =
+          if *escChar == 'n' then '\n'
+          else if *escChar == 'r' then '\r'
+          else if *escChar == 't' then '\t'
+          else *escChar in
+        (*Buf ++ *char, substrRem(*Serial, 2))
+      else (*Buf ++ strHd(*Serial), strTl(*Serial)) in
+    extractString(*Buf, *Serial)
 
 INPUT *Serial=''
 OUTPUT ruleExecOut
